815a54370d4944baba292c5fd6f42a290856f216
Dylan Heinrich <harly13@live.com>
2019-02-19 00:49:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
   var i = 0;
   var test = `${cart[i].itemName}`;
   console.log(test)
-  while(!test.toEquals(item)){
+  while(!test.toEqual(item)){
     i++
     test = `${cart[i].itemName}`;
   }
