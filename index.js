dc2f9e914ff045ee73e1bafb8844097d2f02b9b1
Dylan Heinrich <harly13@live.com>
2019-02-19 00:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if(cart.length === 0){
-    return 'Your shopping cart is empty'
+    return 'Your shopping cart is empty.'
   }
   else if(cart === 1){
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }
 }
 
